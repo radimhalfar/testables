8b1cdf17b025d8e67e7d6b09088dcf49c9cc21d3
Radim Halfar <radim.halfar@inloop.eu>
2017-01-23 15:07:50 +0100

    Initial commit - project setup

---
+++ Testables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testables
+//
+//  Created by Radim Halfar on 23.01.17.
+//  Copyright © 2017 Inloop, s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
